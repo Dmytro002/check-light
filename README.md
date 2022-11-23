354667ea7e605f10af0b2d586302e3b906f4031c
Oleh Haliak <olehhaliak@gmail.com>
2022-11-23 23:52:31 +0200

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # check-light
+
 The project was created to warn people when the lights on their street are turned off
---
+++ src/main/java/org/idksowtware/checklight/CheckLightApplication.java
@@ -0,0 +1,13 @@
+package org.idksowtware.checklight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckLightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckLightApplication.class, args);
+    }
+
+}
